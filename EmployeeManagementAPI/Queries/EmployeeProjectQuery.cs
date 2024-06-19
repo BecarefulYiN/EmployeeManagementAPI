9aa67f3558e6ff9a1b0b923a24cb9b4859658024
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-19 21:16:07 +0630

    -

--- EmployeeManagementAPI/Queries/EmployeeProjectQuery.cs
+++ EmployeeManagementAPI/Queries/EmployeeProjectQuery.cs
@@ -1,44 +1,43 @@
-﻿namespace EmployeeManagementAPI.Queries
+﻿namespace EmployeeManagementAPI.Queries;
+
+public class EmployeeProjectQuery
 {
-    public class EmployeeProjectQuery
+    public static string GetEmployeeProjectListQuery()
     {
-        public static string GetEmployeeProjectListQuery()
-        {
-            return @"SELECT [EmployeeId]
+        return @"SELECT [EmployeeId]
       ,[ProjectId]
   FROM [dbo].[EmployeeProjectTable] 
             WHERE IsActive = @IsActive";
-        }
+    }
 
-        public static string GetCheckEmployeeExistsQuery()
-        {
-            return @"SELECT COUNT(*) FROM [dbo].[EmployeeTable] WHERE EmployeeId = @EmployeeId AND IsActive = @IsActive";
-        }
+    public static string GetCheckEmployeeExistsQuery()
+    {
+        return @"SELECT COUNT(*) FROM [dbo].[EmployeeTable] WHERE EmployeeId = @EmployeeId AND IsActive = @IsActive";
+    }
 
-        public static string GetCheckProjectExistsQuery()
-        {
-            return @"SELECT COUNT(*) FROM [dbo].[ProjectTable] WHERE ProjectId = @ProjectId AND IsActive = @IsActive";
-        }
+    public static string GetCheckProjectExistsQuery()
+    {
+        return @"SELECT COUNT(*) FROM [dbo].[ProjectTable] WHERE ProjectId = @ProjectId AND IsActive = @IsActive";
+    }
 
-        public static string GetCheckDuplicateEmployeeProjectAssociation()
-        {
-            return @"SELECT [EmployeeId], [ProjectId], [IsActive]
+    public static string GetCheckDuplicateEmployeeProjectAssociation()
+    {
+        return @"SELECT [EmployeeId], [ProjectId], [IsActive]
                                       FROM [dbo].[EmployeeProjectTable]
                                       WHERE EmployeeId = @EmployeeId AND ProjectId = @ProjectId AND IsActive = @IsActive";
-        }
+    }
 
-        public static string GetInsertEmployeeProjectQuery()
-        {
-            return @"INSERT INTO [dbo].[EmployeeProjectTable]
+    public static string GetInsertEmployeeProjectQuery()
+    {
+        return @"INSERT INTO [dbo].[EmployeeProjectTable]
                              ([EmployeeId], [ProjectId], [IsActive])
                              VALUES (@EmployeeId, @ProjectId, @IsActive)";
-        }
+    }
 
-        public static string GetDeleteEmployeeProjectQuery()
-        {
-            return @"UPDATE [dbo].[EmployeeProjectTable]
+    public static string GetDeleteEmployeeProjectQuery()
+    {
+        return @"UPDATE [dbo].[EmployeeProjectTable]
    SET IsActive = @IsActive
  WHERE ProjectId = @ProjectId AND EmployeeId = @EmployeeId";
-        }
     }
 }
