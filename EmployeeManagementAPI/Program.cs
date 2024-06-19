37fc2553c07d1d32652135c21f955b15997eb632
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-19 21:44:30 +0630

    [Added] Modular Service

--- EmployeeManagementAPI/Program.cs
+++ EmployeeManagementAPI/Program.cs
@@ -2,30 +2,13 @@ using EmployeeManagementAPI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllers().AddJsonOptions(opt =>
-{
-    opt.JsonSerializerOptions.PropertyNamingPolicy = null;
-});
+builder.Services.AddFeatures(builder);
 
-// Add CORS policy
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowLocalhost3000",
-        builder => builder.WithOrigins("http://localhost:3000")
-                          .AllowAnyMethod()
-                          .AllowAnyHeader());
-});
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<AdoDotNetServices>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -34,7 +17,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-// Use CORS policy
 app.UseCors("AllowLocalhost3000");
 
 app.UseAuthorization();
---
+++ EmployeeManagementAPI/Services/ModularService.cs
@@ -0,0 +1,40 @@
+﻿namespace EmployeeManagementAPI.Services
+{
+    public static class ModularService
+    {
+        public static IServiceCollection AddFeatures(this IServiceCollection services, WebApplicationBuilder builder)
+        {
+            services.AddJsonServices();
+            services.AddCorsService(builder);
+            services.AddCustomServices();
+            return services;
+        }
+
+        private static IServiceCollection AddJsonServices(this IServiceCollection services)
+        {
+            services.AddControllers().AddJsonOptions(opt =>
+            {
+                opt.JsonSerializerOptions.PropertyNamingPolicy = null;
+            });
+            return services;
+        }
+
+        private static IServiceCollection AddCorsService(this IServiceCollection services, WebApplicationBuilder builder)
+        {
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowLocalhost3000",
+                    builder => builder.WithOrigins("http://localhost:3000")
+                                      .AllowAnyMethod()
+                                      .AllowAnyHeader());
+            });
+            return services;
+        }
+
+        private static IServiceCollection AddCustomServices(this IServiceCollection services)
+        {
+            services.AddScoped<AdoDotNetServices>();
+            return services;
+        }
+    }
+}
